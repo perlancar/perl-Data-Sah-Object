43a70fe63a228ad822e8e15358d8d2661ff76bf5
Steven Haryanto (on Asus Zenbook) <stevenharyanto@gmail.com>
2014-07-28 00:54:16 +0700

    Initial version

---
+++ lib/Data/Sah/Object.pm
@@ -0,0 +1,66 @@
+package Data::Sah::Object;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+require Exporter;
+our @ISA    = qw(Exporter);
+our @EXPORT = qw(sah sahn);
+
+sub sah {
+    require Data::Sah::Object::Schema;
+    Data::Sah::Object::Schema->new($_[0]);
+}
+
+sub sahn {
+    require Data::Sah::Object::Schema;
+    Data::Sah::Object::Schema->new($_[0], 1);
+}
+
+1;
+# ABSTRACT: Object-oriented interface for Sah schemas
+
+=head1 SYNOPSIS
+
+ use Data::Sah::Object; # automatically exports sah(), sahn()
+
+ # sah() creates a normalized copy of schema
+ $osch = sah("array*");
+ $osch = sah(['array*', of => 'str*']);
+
+ # sahn() assumes you're passing an already-normalized schema and it will not
+ # create a copy
+ my $osch = sahn([array => {req=>1, of=>'str*'}, {}]);
+
+ say $osch->type; # -> array
+ say $osch->clause('req'); # -> 1
+ say $osch->clause('of', 'int'); # set clause
+
+
+=head1 DESCRIPTION
+
+L<Sah> works using pure data structures, but sometimes it's convenient to have
+an object-oriented interface (wrapper) for those data. This module provides just
+that.
+
+
+=head1 FUNCTIONS
+
+=head2 sah $sch => OBJECT
+
+Exported by default. A shortcut for Data::Sah::Object::Schema->new($sch).
+
+=head2 sahn $sch => OBJECT
+
+Exported by default. A shortcut for Data::Sah::Object::Schema->new($sch, 1).
+
+
+=head1 SEE ALSO
+
+L<Sah>, L<Data::Sah>
+
+=cut
---
+++ lib/Data/Sah/Object/Schema.pm
@@ -0,0 +1,98 @@
+package Data::Sah::Object::Schema;
+
+# DATE
+# VERSION
+
+use 5.010;
+use strict;
+use warnings;
+
+sub new {
+    my ($class, $sch, $is_normalized) = @_;
+    $sch //= [undef, {}, {}];
+
+    unless ($is_normalized) {
+        require Data::Sah::Normalize;
+        $sch = Data::Sah::Normalize::normalize_schema($sch);
+    }
+
+    my $obj = \$sch;
+    bless $obj, $class;
+}
+
+sub type {
+    my $self = shift;
+    if (@_) {
+        my $old = ${$self}->[0];
+        ${$self}->[0] = $_[0];
+        return $old;
+    } else {
+        return ${$self}->[0];
+    }
+}
+
+sub clause {
+    my $self = shift;
+    my $name = shift;
+    if (@_) {
+        my $old = ${$self}->[1]{$name};
+        ${$self}->[1]{$name} = $_[0];
+        return $old;
+    } else {
+        return ${$self}->[1]{$name};
+    }
+}
+
+sub req {
+    my $self = shift;
+    if (@_) {
+        my $old = ${$self}->[1]{req};
+        ${$self}->[1]{req} = $_[0];
+        return $old;
+    } else {
+        return ${$self}->[1]{req};
+    }
+}
+
+sub delete_clause {
+    my $self = shift;
+    my $name = shift;
+    my $old = ${$self}->[1]{$name};
+    delete ${$self}->[1]{$name};
+    return $old;
+}
+
+sub as_struct {
+    my $self = shift;
+    ${$self};
+}
+
+1;
+# ABSTRACT: Represent Sah schema
+
+=head1 METHODS
+
+=head2 new($sch, $is_normalized) => obj
+
+Constructor. Will create a normalized copy of C<$sch>, unless C<$is_normalized>
+is true, in which case won't create a copy.
+
+=head2 type([ $value ]) => str
+
+Get or set type. If set, will return the old value.
+
+=head2 clause($name[, $value]) => any
+
+Get or set a clause. If set, will return the old value.
+
+=head2 req([ $value ]) => bool
+
+Shortcut for C<< $osch->clause('req'[, $value ]) >>.
+
+=head2 delete_clause($name) => any
+
+Delete clause named C<$name>. Return the old value.
+
+=back
+
+=cut
